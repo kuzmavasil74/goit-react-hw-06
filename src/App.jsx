fcb561ca464f08a063db14d78b48fff4ef543b97
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-04-14 23:02:41 +0200

    finish tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
 import ContactForm from './components/ContactForm/ContactForm'
 import ContactList from './components/ContactList/ContactList'
 import SearchBox from './components/SearchBox/SearchBox'
-import contactsData from './contacts.json'
 import Contact from './components/Contacts/Contacts'
-import { useEffect, useState } from 'react'
 import { nanoid } from 'nanoid'
 import css from './App.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { addContact, deleteContact } from './redux/contactsSlice'
+import { setFilterContact } from './redux/filtersSlice'
 
 function App() {
-  const [contactsList, setContactsList] = useState(() => {
-    const stringingifiedContacts = localStorage.getItem('contacts')
-    if (!stringingifiedContacts) return contactsData
-    return JSON.parse(stringingifiedContacts)
+  const dispatch = useDispatch()
+  const contacts = useSelector((state) => state.contact.contacts.items)
+  const filter = useSelector((state) => {
+    console.log(state)
+    return state.filter.filters.name
   })
-
-  const [filter, setFilter] = useState('')
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contactsList))
-  }, [contactsList])
 
   const onAddContact = (formData) => {
     const finalContact = {
       ...formData,
       id: nanoid(),
     }
-
-    setContactsList((prevState) => [...prevState, finalContact])
+    const action = addContact(finalContact)
+    dispatch(action)
   }
 
   const onDeleteContact = (contactId) => {
-    setContactsList((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    )
+    const action = deleteContact(contactId)
+    dispatch(action)
   }
   const onChangeFilter = (event) => {
-    setFilter(event.target.value)
+    dispatch(setFilterContact(event.target.value))
   }
 
-  const filteredContacts = contactsList.filter(
-    (contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-      contact.number.includes(filter)
-  )
+  const filteredContacts = contacts.filter((contact) => {
+    return (
+      (contact.name &&
+        contact.name
+          .toLowerCase()
+          .includes(filter ? filter.toLowerCase() : '')) ||
+      (contact.number && contact.number.includes(filter ? filter : ''))
+    )
+  })
 
   return (
     <div>
