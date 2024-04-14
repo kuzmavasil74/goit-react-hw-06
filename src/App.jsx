5463918ac8b227c6e8dfe36894f4c5d19ae367f6
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-04-14 18:30:17 +0200

    comy base information from hw03

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import './App.module.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactList from './components/ContactList/ContactList'
+import SearchBox from './components/SearchBox/SearchBox'
+import contactsData from './contacts.json'
+import Contact from './components/Contacts/Contacts'
+import { useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
+import css from './App.module.css'
 
 function App() {
+  const [contactsList, setContactsList] = useState(() => {
+    const stringingifiedContacts = localStorage.getItem('contacts')
+    if (!stringingifiedContacts) return contactsData
+    return JSON.parse(stringingifiedContacts)
+  })
+
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contactsList))
+  }, [contactsList])
+
+  const onAddContact = (formData) => {
+    const finalContact = {
+      ...formData,
+      id: nanoid(),
+    }
+
+    setContactsList((prevState) => [...prevState, finalContact])
+  }
+
+  const onDeleteContact = (contactId) => {
+    setContactsList((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    )
+  }
+  const onChangeFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredContacts = contactsList.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  )
+
   return (
     <div>
-      <h1>React template for home work</h1>
+      <h1 className={css.appTitle}>Phonebook</h1>
+      <ContactForm onAddContact={onAddContact} />
+      <SearchBox filter={filter} onChangeFilter={onChangeFilter} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={onDeleteContact}
+      />
+      <Contact />
     </div>
   )
 }
